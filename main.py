8403e38eea79154198d5e0b1b6a69a43d35b5f42
Imbiridin <marcosimbiriba1@gmail.com>
2026-05-13 19:29:50 -0300

    implementa cadastro de venda

--- main.py
+++ main.py
@@ -18,12 +18,27 @@ if usuario == 1:
     
     valor_bruto = valor_u * quant
 
+    if valor_bruto >= 1000:
+        desconto = 0.15
+        desconto_valor = valor_bruto * desconto
+    elif valor_bruto < 1000 or valor_bruto >= 500:
+        desconto = 0.10
+        desconto_valor = valor_bruto * desconto
+    elif valor_bruto < 500 or valor_bruto >= 100:
+        desconto = 0.05
+        desconto_valor = valor_bruto * desconto
+    else: 
+        print("Não tem desconto!")
+
 elif usuario == 2:
     print("2- Ver resumo Parcial")
 elif usuario == 3:
     print("3 - Encerrar Sistema")
 
 
-print("Valor Bruto da venda é: " , valor_bruto)
+print(f"Valor Bruto da venda é: R$ {valor_bruto:.2f}")
+print(f"O desconto aplicado é: {desconto:.2f} %"  )
+print(f"Valor do desconto é: R$ {desconto_valor:.2f}")
+
 
 
